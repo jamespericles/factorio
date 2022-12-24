20dafbb0baa1ad4f4c65534c2881bacbab66ec86
James Pericles <james@Amandas-Air.webpass.net>
2022-12-24 00:08:03 -0600

    Update

--- main.sh
+++ main.sh
@@ -8,16 +8,21 @@ die () {
 
 [ "$#" -eq 1 ] || die "1 argument required, $# provided"
 
+[ -d "$factorioLocal" ] || die "Factorio local directory does not exist"
+[ -d "$factorioRepo" ] || die "Factorio repo directory does not exist"
+[ -d "$factorioBackup" ] || die "Factorio backup directory does not exist"
+
 # validate the current argument was provided, either push or pull
 if [ "$1" = "push" ] || [ "$1" = "pull" ] || [ "$1" = "update" ]; then
     echo "Valid argument provided"
     # If the argument is push, then copy all files from LOCAL and push to github
     if [ "$1" = "push" ]; then
-        echo "Pushing to github"
+        echo "Preparing directories..."
+        echo "Coping from ${factorioLocal} to ${factorioBackup}..."
         cp -r "$factorioLocal"/* $factorioBackup
         echo "Backed up local files"
+        echo "Pushing to github..."
         cp -r "$factorioLocal"/* $factorioRepo
-        echo "Copied local files to repo dir"
         git add .
         git commit -m "Update"
         git push
@@ -26,6 +31,7 @@ if [ "$1" = "push" ] || [ "$1" = "pull" ] || [ "$1" = "update" ]; then
     elif [ "$1" = "pull" ]; then
         echo "Pulling from github"
         git pull
+        echo "Coping from ${factorioRepo} to ${factorioLocal}..."
         cp -r $factorioRepo/* "$factorioLocal"
         echo "Updated local files"
     fi
